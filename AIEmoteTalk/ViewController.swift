63fbba72bdfbe350c492e1b079ae711264124fc6
mue9997 <mue778@gmail.com>
2023-05-01 16:29:35 +0900

    Initial Commit

---
+++ AIEmoteTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIEmoteTalk
+//
+//  Created by 瀧澤宗茂 on 2023/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
